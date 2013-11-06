2719b3fedbfd5ec6608f24ad25c96eefef82ad81
christianseel <cs@chsmedien.de>
2013-11-06 12:16:19 +0100

    Revert "added german translation"
    This reverts commit 149a30636ec42980f0a6fc792f4f858858bb130a.

--- core/components/seopro/lexicon/de/default.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-$_lang['seopro.keywords'] = 'Keywords';
-$_lang['seopro.characters'] = 'Zeichen';
-$_lang['seopro.characters.allowed'] = 'Erlaubte Zeichen';
-$_lang['seopro.tips'] = 'SEO Tipps';
-$_lang['seopro.focuskeywords'] = 'Keywords für diese Seite';
-$_lang['seopro.focuskeywords_desc'] = 'durch Komma getrennt, ohne Leerzeichen';
-$_lang['seopro.prevbox'] = 'Google-Vorschau';
