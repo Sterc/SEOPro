59d683a2695ebb51b64633258709a8fdd5d843cc
Bumme <wiegersloot@gmail.com>
2013-11-06 13:31:28 +0100

    Added Russian lexicon

---
+++ core/components/seopro/lexicon/ru/default.inc.php
@@ -0,0 +1,8 @@
+<?php
+$_lang['seopro.keywords'] = 'Ключевые фразы';
+$_lang['seopro.characters'] = 'Символы';
+$_lang['seopro.characters.allowed'] = 'Разрешено символов';
+$_lang['seopro.tips'] = 'SEO-PRO Советы!';
+$_lang['seopro.focuskeywords'] = 'Фокусные фразы';
+$_lang['seopro.focuskeywords_desc'] = 'Разделённые запятой';
+$_lang['seopro.prevbox'] = 'Как это будет выглядеть в Google?';
