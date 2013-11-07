feb02e9383db8a18331c0af035eabc98027cbcd4
MrAgr3ssive <ershov.ilya@gmail.com>
2013-11-07 10:39:25 +0400

    Added RU localization
    Need to fix size of label "keywords" - make it adaptive, or wider

---
+++ core/components/seopro/lexicon/ru/default.inc.php
@@ -0,0 +1,8 @@
+<?php
+$_lang['seopro.keywords'] = 'keywords'; // Need to be 'Ключевые фразы' // but it is too long for current width of label
+$_lang['seopro.characters'] = 'Символы';
+$_lang['seopro.characters.allowed'] = 'Разрешено символов';
+$_lang['seopro.tips'] = 'SEO-PRO Советы!';
+$_lang['seopro.focuskeywords'] = 'Фокусные фразы';
+$_lang['seopro.focuskeywords_desc'] = 'Разделённые запятой';
+$_lang['seopro.prevbox'] = 'Как это будет выглядеть в Google?';
