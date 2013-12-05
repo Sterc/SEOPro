a5776c99e339da6e2867a746cf2f35ffe8244dc3
Wieger Sloot <wiegersloot@gmail.com>
2013-12-05 09:33:53 +0100

    Update dutch lexicon

--- core/components/seopro/lexicon/nl/default.inc.php
+++ core/components/seopro/lexicon/nl/default.inc.php
@@ -4,5 +4,5 @@ $_lang['seopro.characters'] = 'Karakters';
 $_lang['seopro.characters.allowed'] = 'Toegestaande karakters';
 $_lang['seopro.tips'] = 'SEO-PRO Tips!';
 $_lang['seopro.focuskeywords'] = 'Focus keywords';
-$_lang['seopro.focuskeywords_desc'] = 'Komma gescheiden invullen';
+$_lang['seopro.focuskeywords_desc'] = 'Vul uw keywords komma gescheiden in.';
 $_lang['seopro.prevbox'] = 'Voorbeeld in Google';
