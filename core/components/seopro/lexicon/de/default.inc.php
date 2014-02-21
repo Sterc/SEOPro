1f0e01c2fd0c691281ad0d35f1f1df3433cb0a2b
Christian Seel <cs@chsmedien.de>
2014-02-21 10:39:16 +0100

    updated german translation

--- core/components/seopro/lexicon/de/default.inc.php
+++ core/components/seopro/lexicon/de/default.inc.php
@@ -8,5 +8,13 @@ $_lang['seopro.focuskeywords_desc'] = 'durch Komma getrennt, ohne Leerzeichen';
 $_lang['seopro.prevbox'] = 'Google-Vorschau';
 $_lang['seopro.emptymetadescription']='<i>Bitte geben Sie eine Beschreibung ein.</i>';
 
-$_lang['setting_seopro.delimiter'] = 'Trennzeichen in Google-Vorschau.';
-$_lang['setting_seopro.delimiter_desc'] = 'Trennzeichen zwischen Titel und Seitenname.';
+$_lang['setting_seopro.delimiter'] = 'Trennzeichen in Google-Vorschau';
+$_lang['setting_seopro.delimiter_desc'] = 'Trennzeichen zwischen Titel und Seitenname';
+$_lang['setting_seopro.fields'] = 'SEO Pro Felder';
+$_lang['setting_seopro.fields_desc'] = 'Ändern Sie diese Einstellung nur, wenn Sie genau wissen, was Sie tun. Standard-Felder: pagetitle:70,longtitle:70,description:155,alias:2023,menutitle:2023. SEO Pro funktioniert nicht für das Content-Feld.';
+$_lang['setting_seopro.version'] ='Versionsnummer';
+$_lang['setting_seopro.version_desc']= 'Aktuelle '.$_lang['setting_seopro.version'];
+$_lang['setting_seopro.allowbranding'] ='Sterc Branding im Head erlauben';
+$_lang['setting_seopro.allowbranding_desc']= 'Give us some love...';
+$_lang['setting_seopro.usesitename'] ='site_name im Titel verwenden?';
+$_lang['setting_seopro.usesitename_desc']= 'Wenn Sie NEIN auswählen wird der site_name und das Trennzeichen aus der Vorschau und dem SEO Pro Platzhalter ([[+seoPro.title]]) entfernt.';
