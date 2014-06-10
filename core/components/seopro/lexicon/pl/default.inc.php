de263a313bda6e193a818d05293abe81b7fa8066
Gadoma <gadoma@gadoma.info>
2014-06-10 03:04:10 +0200

    Polish language translation file

---
+++ core/components/seopro/lexicon/pl/default.inc.php
@@ -0,0 +1,20 @@
+<?php
+$_lang['seopro.keywords'] = 'Frazy kluczowe';
+$_lang['seopro.characters'] = 'Liczba znaków';
+$_lang['seopro.characters.allowed'] = 'Maksymalna liczba znaków';
+$_lang['seopro.tips'] = 'SEO-PRO Tips!';
+$_lang['seopro.focuskeywords'] = 'Frazy fokusowe';
+$_lang['seopro.focuskeywords_desc'] = 'Wprowadź frazy oddzielone przecinkami (przykład: stół,krzesło)';
+$_lang['seopro.prevbox'] = 'Jak to wygląda w Google?';
+$_lang['seopro.emptymetadescription']='<i>Wprowadź opis aby zobaczyć podgląd</i>';
+
+$_lang['setting_seopro.delimiter'] = 'Separator';
+$_lang['setting_seopro.delimiter_desc'] = 'Separator pomiędzy title i sitename';
+$_lang['setting_seopro.fields'] = 'Pola na których będzie operować SEO Pro';
+$_lang['setting_seopro.fields_desc'] = 'Modyfikuj ustawienie tylko jeśli wiesz co robisz. Domyślna wartość:pagetitle,longtitle,description,alias,menutitle. Nie działa dla pola content.';
+$_lang['setting_seopro.version'] ='Numer wersji';
+$_lang['setting_seopro.version_desc']= 'Bieżąca '.$_lang['setting_seopro.version'];
+$_lang['setting_seopro.allowbranding'] ='Pozwól na branding Sterc w kodzie strony';
+$_lang['setting_seopro.allowbranding_desc']= 'Zezwól na link do naszej strony ukryty w komentarzu HTML';
+$_lang['setting_seopro.usesitename'] ='Pokaż sitename w [[+seoPro.title]] oraz w podglądzie Google?';
+$_lang['setting_seopro.usesitename_desc']= 'Wartość NO oznacza usunięcie sitename z [[+seoPro.title]] i podglądu Google';
