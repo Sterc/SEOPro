07187f3c8f2e5f13c83724c73460cf5fbaf59aa8
Joeke Kloosterman <joeke@sterc.nl>
2017-04-05 23:09:51 +0200

    Refactor assigning and checking of template resource/request variable (fixes #58 and #59)

--- core/components/seopro/elements/plugins/seopro.plugin.php
+++ core/components/seopro/elements/plugins/seopro.plugin.php
@@ -13,10 +13,17 @@ $disabledTemplates = explode(',', $modx->getOption('seopro.disabledtemplates', n
 
 switch ($modx->event->name) {
     case 'OnDocFormRender':
-        $template = ($resource->get('template')) ? (string)$resource->get('template') : (string)$_REQUEST['template'];
-        if (!empty($template) && in_array($template, $disabledTemplates)) {
+        $template = (string)$resource->get('template');
+        $override = false;
+        if (isset($_REQUEST['template'])) {
+            $template = (string)$_REQUEST['template'];
+            $override = true;
+        }
+        if (($override && $template === '0') || (!empty($template) && in_array($template, $disabledTemplates))) {
             break;
         }
+        
+        
         $currClassKey = $resource->get('class_key');
         $strFields = $modx->getOption('seopro.fields', null, 'pagetitle:70,longtitle:70,description:155,alias:2023,menutitle:2023');
         $arrFields = array();
