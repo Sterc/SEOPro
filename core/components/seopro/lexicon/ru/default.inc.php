e050607e40d54143dd28c45d29ac35b2574de402
Bochkarev Ivan <ivanx86@gmail.com>
2019-03-21 03:26:58 +0600

    Update Russian translation

--- core/components/seopro/lexicon/ru/default.inc.php
+++ core/components/seopro/lexicon/ru/default.inc.php
@@ -2,16 +2,34 @@
 $_lang['seopro.keywords'] = 'Ключевые фразы';
 $_lang['seopro.characters'] = 'Символы';
 $_lang['seopro.characters.allowed'] = 'Разрешено символов';
-$_lang['seopro.tips'] = 'SEO-PRO Советы!';
+$_lang['seopro.tips'] = 'SEO Pro советы';
 $_lang['seopro.focuskeywords'] = 'Фокусные фразы';
-$_lang['seopro.focuskeywords_desc'] = 'Разделённые запятой';
+$_lang['seopro.focuskeywords_desc'] = 'Введите ключевые слова через запятую (пример: стол, стул)';
 $_lang['seopro.prevbox'] = 'Как это будет выглядеть в Google?';
 $_lang['seopro.prevbox_yandex'] = 'Как это будет выглядеть в Yandex?';
-$_lang['seopro.emptymetadescription']='<i>Для предварительного просмотра заполните поле Описание</i>';
-$_lang['seopro.branding_text']='This site is optimized with the Sterc seoPro plugin - https://github.com/Sterc/SEOPro.';
+$_lang['seopro.emptymetadescription']='<i>Пожалуйста введите описание</i>';
+$_lang['seopro.branding_text']='Этот сайт оптимизирован с Sterc seoPro плагином - https://github.com/Sterc/SEOPro.';
 
-$_lang['setting_seopro.delimiter'] = 'Разделитель в сниппете Google/Yandex';
+$_lang['setting_seopro.delimiter'] = 'Разделитель в сниппете Google/Yandex.';
 $_lang['setting_seopro.delimiter_desc'] = 'Разделитель между Заголовком и Названием сайта';
-$_lang['setting_seopro.title_format'] ='Format used for the meta title.';
-$_lang['setting_seopro.title_format_desc'] ='Here you can specify the format used for the meta title used
- in Google/Yandex preview. The rendered output is also available on frontend with [[+seoPro.title]].';
+$_lang['setting_seopro.fields'] = 'Поля, где будет работать SEO Pro';
+$_lang['setting_seopro.fields_desc'] = 'Измените их, только если вы знаете, что делаете. Значение по умолчанию: pagetitle,longtitle,description,alias,menutitle. Не будет работать на поле контента';
+$_lang['setting_seopro.version'] ='Номер версии';
+$_lang['setting_seopro.version_desc']= 'Текущий '.$_lang['setting_seopro.version'];
+$_lang['setting_seopro.allowbranding'] ='Разрешить брендинг Sterc в &lt;head&gt;';
+$_lang['setting_seopro.allowbranding_desc']= 'Покажите нам немного любви...';
+$_lang['setting_seopro.usesitename'] ='Отобразите sitename в Google/Yandex просмотре и добавьте его в заголовок для SEO Pro. [[+seoPro.title]]?';
+$_lang['setting_seopro.usesitename_desc']= 'Если это значение равно \'no\', разделитель и настройка имени сайта будут удалены из местозаполнителя заголовка Google Preview AND SEO Pro.';
+$_lang['setting_seopro.disabledtemplates'] ='Отключенные шаблоны';
+$_lang['setting_seopro.disabledtemplates_desc']= 'Разделенный запятыми список отключенных шаблонов IDs';
+$_lang['setting_seopro.max_keywords_title'] ='Максимальное количество ключевых слов в фокусе, разрешенных в заголовке.';
+$_lang['setting_seopro.max_keywords_title_desc'] ='Используйте этот параметр, чтобы контролировать максимальное количество ключевых слов для фокусировки, которые разрешены в заголовке, чтобы получить одобрение.
+  Рекомендуется использовать не более 4 ключевых слов в заголовке.';
+$_lang['setting_seopro.max_keywords_description'] ='Максимальное количество ключевых слов фокуса, разрешенных в описании.';
+$_lang['setting_seopro.max_keywords_description_desc'] ='Используйте этот параметр, чтобы контролировать максимальное количество ключевых слов для фокусировки, разрешенных в описании, чтобы получить одобрение.
+  Рекомендуется использовать не более 8 ключевых слов в вашем описании..';
+$_lang['setting_seopro.searchengine'] ='Поисковая система, используемая для предварительного просмотра.';
+$_lang['setting_seopro.searchengine_desc'] ='Возможные значения: google, yandex. По умолчанию Google.';
+$_lang['setting_seopro.title_format'] ='Формат, используемый для мета-заголовка.';
+$_lang['setting_seopro.title_format_desc'] ='Здесь вы можете указать формат для мета заголовка
+  в превью Google/Yandex. Рендеринг вывода также доступен на веб-интерфейсе с [[+seoPro.title]].';
